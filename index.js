90ec6e7b483116c1941ff0d641d98332f658cd86
viqi zevic <vicky.tanzil@gmail.com>
2018-09-02 20:01:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function takeANumber (deli, name) {
   deli.push(name)
   return `Welcome, ${name}. You are number ${deli.length} in line.`
 }
+
+function nowServing(deli) {
+  name = deli.pop()
+  return name
+}
